21a1d5fbf10e195f4bb1678a9d5340055147d4e7
Vinicius Ribeiro <viniribeiroa@gmail.com>
2023-10-02 00:06:54 -0300

    caonfiguraçao FirebaseAuth e Usuario

--- app/src/main/java/com/stormdev/organizze/activity/CadastroActivity.java
+++ app/src/main/java/com/stormdev/organizze/activity/CadastroActivity.java
@@ -1,16 +1,81 @@
 package com.stormdev.organizze.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.QuickContactBadge;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.stormdev.organizze.R;
+import com.stormdev.organizze.config.ConfiguracaoFirebase;
+import com.stormdev.organizze.model.Usuario;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private EditText campoNome, campoEmail, campoSenha;
+    private Button botaoCadastrar;
+    private FirebaseAuth autenticacao;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        campoNome = findViewById(R.id.editNome);
+        campoEmail = findViewById(R.id.editEmail);
+        campoSenha = findViewById(R.id.editSenha);
+        botaoCadastrar = findViewById(R.id.buttonCadastrar);
+
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textoNome = campoNome.getText().toString();
+                String textoEmail = campoEmail.getText().toString();
+                String textoSenha = campoSenha.getText().toString();
+
+                //validar se campos foram preeechidos
+                if (!textoNome.isEmpty()){
+                    if (!textoEmail.isEmpty()){
+                        if (!textoSenha.isEmpty()){
+                            usuario =new Usuario();
+                            usuario.setNome(textoNome);
+                            usuario.setEmail(textoEmail);
+                            usuario.setSenha(textoSenha);
+                            cadastraUsuario();
+                        }else {
+                            Toast.makeText(CadastroActivity.this, "Preencha a senha", Toast.LENGTH_LONG).show();
+                        }
+                    }else {
+                        Toast.makeText(CadastroActivity.this, "Preencha o email", Toast.LENGTH_LONG).show();
+                    }
+                }else {
+                    Toast.makeText(CadastroActivity.this, "Preencha o nome", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    public void cadastraUsuario(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.createUserWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(CadastroActivity.this, "Sucesso ao cadastrar usuario!", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(CadastroActivity.this, "Erro ao cadastrar usuario!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
