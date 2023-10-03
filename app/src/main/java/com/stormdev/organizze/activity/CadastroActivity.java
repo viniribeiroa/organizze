b06803c388a1a75c04bc16e554f3ee015210869a
Vinicius Ribeiro <viniribeiroa@gmail.com>
2023-10-02 22:55:33 -0300

    tratamento de exceções

--- app/src/main/java/com/stormdev/organizze/activity/CadastroActivity.java
+++ app/src/main/java/com/stormdev/organizze/activity/CadastroActivity.java
@@ -14,6 +14,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.stormdev.organizze.R;
 import com.stormdev.organizze.config.ConfiguracaoFirebase;
 import com.stormdev.organizze.model.Usuario;
@@ -73,7 +76,20 @@ public class CadastroActivity extends AppCompatActivity {
                 if (task.isSuccessful()){
                     Toast.makeText(CadastroActivity.this, "Sucesso ao cadastrar usuario!", Toast.LENGTH_SHORT).show();
                 }else {
-                    Toast.makeText(CadastroActivity.this, "Erro ao cadastrar usuario!", Toast.LENGTH_SHORT).show();
+                    String excecao = "";
+                    try{
+                        throw task.getException();
+                    }catch (FirebaseAuthWeakPasswordException e){
+                        excecao = "Digite uma senha mais forte";
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        excecao = "Digite um e-mail valido";
+                    }catch (FirebaseAuthUserCollisionException e){
+                        excecao = "Essa conta já foi cadastrada";
+                    }catch (Exception e){
+                        excecao = "Erro ao cadastrar usuário: " + e.getMessage();
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(CadastroActivity.this, excecao, Toast.LENGTH_SHORT).show();
                 }
             }
         });
--- app/src/main/java/com/stormdev/organizze/activity/LoginActivity.java
+++ app/src/main/java/com/stormdev/organizze/activity/LoginActivity.java
@@ -1,16 +1,90 @@
 package com.stormdev.organizze.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.stormdev.organizze.R;
+import com.stormdev.organizze.config.ConfiguracaoFirebase;
+import com.stormdev.organizze.model.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText campoEmail, campoSenha;
+    private Button botaoEntrar;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        campoEmail = findViewById(R.id.editEmailLogin);
+        campoSenha = findViewById(R.id.editSenhaLogin);
+        botaoEntrar = findViewById(R.id.buttonEntrar);
+
+        botaoEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String textoEmail = campoEmail.getText().toString();
+                String textoSenha = campoSenha.getText().toString();
+
+                if (!textoEmail.isEmpty()){
+                    if (!textoSenha.isEmpty()){
+
+                        usuario = new Usuario();
+                        usuario.setEmail(textoEmail);
+                        usuario.setSenha(textoSenha);
+                        validarLogin();
+
+                    }else {
+                        Toast.makeText(LoginActivity.this, "Preencha sua senha", Toast.LENGTH_SHORT).show();
+                    }
+                }else {
+                    Toast.makeText(LoginActivity.this, "Preecha um e-mail valido", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    public void validarLogin(){
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Sucesso Ao fazer Login", Toast.LENGTH_SHORT).show();
+                }else {
+
+                    String excecao;
+                    try {
+                        throw  task.getException();
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        excecao = "E-mail e senha não correspondem a um usuario cadastrado";
+                    }catch (FirebaseAuthInvalidUserException e){
+                        excecao = "Usuario não está cadastrado";
+                    }catch (Exception e){
+                        excecao = "Error ao logar: " + e.getMessage();
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(LoginActivity.this, excecao, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
